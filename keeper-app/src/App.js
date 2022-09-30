ca7ca980ec65b8b9d04bcf34092317a2bf3de3f7
faheem4531 <aa0561085@gmail.com>
2022-09-30 11:56:01 +0500

    maping

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -3,13 +3,22 @@ import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Note from "./components/note/Note";
+import Detail from "./components/note/Details";
 
 const App = () => {
   return (
     <div>
-    <Header/>
-    <Note/>
-    <Footer/>
+      <Header />
+
+      {Detail.map(obj =>
+        <Note
+          key={obj.key}
+          heading={obj.title}
+          detail={obj.content}
+        />
+      )
+      }
+      <Footer />
     </div>
   );
 }
--- keeper-app/src/components/note/Note.js
+++ keeper-app/src/components/note/Note.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Note = () => {
-    return(
-<div className="note">
-    <h1>title</h1>
-    <p>context</p>
-</div>
+const Note = (prop) => {
+    return (
+        <div className="note">
+            <h1>{prop.heading}</h1>
+            <p>{prop.detail}</p>
+        </div>
     )
 }
 
