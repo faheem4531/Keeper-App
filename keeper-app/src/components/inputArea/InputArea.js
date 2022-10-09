e6e60c1c6ab14412ca275436deadf08aeb5f8d3a
faheem4531 <aa0561085@gmail.com>
2022-10-09 21:30:34 +0500

    i-Create a constant that keeps title and content.

--- keeper-app/src/components/inputArea/InputArea.js
+++ keeper-app/src/components/inputArea/InputArea.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InputArea() {
+
+  const [note, setNote] = useState({
+    title: "",
+    text: ""
+  })
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          name="title"
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          value={note.text}
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
